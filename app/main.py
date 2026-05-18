d59d7e8f8f8655d96eabd218fde93f951252fe53
lililhuan <[rollesis@my.cspc.edu.ph]>
2026-05-18 18:33:31 +0800

    workflows

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/health")
 def health_check():
     return jsonify({"status": "ok"}), 200
+
 
 @app.route("/")
 def home():
